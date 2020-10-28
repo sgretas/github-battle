a8beace2764969941a3baa20bee761e733e140e3
Simon Gretas <simon.gretas@vgq.qc.ca>
2020-10-28 06:27:08 -0400

    Toggle theme and Nav component created

--- app/index.js
+++ app/index.js
@@ -4,9 +4,10 @@ import "./index.css";
 import Popular from "./components/Popular";
 import Battle from "./components/Battle";
 import { ThemeProvider } from "./contexts/theme";
+import Nav from './components/Nav';
 
 class App extends React.Component {
-  constructor() {
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -20,9 +21,12 @@ class App extends React.Component {
   }
   render() {
     return (
-      <ThemeProvider value={}>
-        <div className="container">
-          <Battle />
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className="container">
+            <Nav />
+            <Battle />
+          </div>
         </div>
       </ThemeProvider>
     );
